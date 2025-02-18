0cbd66710874797077b8924441e9529e469aa284
Akinyemi <aki.b@pentridgemedia.com>
2025-02-18 01:14:48 -0500

    Fix PostCSS config: Update dependencies and configuration

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,5 +1,11 @@
+// @ts-check
+
+/**
+ * @type {import('postcss').Postcss}
+ */
 module.exports = {
   plugins: {
+    'tailwindcss/nesting': {},
     'tailwindcss': {},
     'autoprefixer': {},
   }
